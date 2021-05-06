cf6f6426671bb4f8a0716b8dcfe7e8d48541f5e1
minirachel <rachel.a.levinson@gmail.com>
2021-05-06 18:21:01 -0400

    commit tree d0dd40388de3e267c574291fd5682c2016003916

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 15, "Male", "brown", "smooth", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 4, "Male", "auburn", "rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 8, "Female", "chocolate", "sparkly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 22, "Male","grey", "wise", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 13, "Female", "Coffee", "warm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 7, "Male", "green", "welcoming", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 15, "Female", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, 6, "Male", "brown", "angry", 1);
